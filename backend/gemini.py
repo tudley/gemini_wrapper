3c179d7c8026e5bf9bdbc0f57b1bfc6848c67f43
Tom Young <youngthomas1997@gmail.com>
2026-07-12 11:35:12 +0100

    Gemini tool successfully added

--- backend/gemini.py
+++ backend/gemini.py
@@ -11,27 +11,62 @@ def main():
         if (not api_key):
             return
 
+    todos = ["Shower"]
+
+    def add_todo(item: str):
+        todos.append(item)
+
+
+
+    add_todo_tool = {
+        "type": "function",
+        "name": "add_todo",
+        "description": "Adds an item to the user's todo list.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "item": {
+                    "type": "string",
+                    "description": "The todo item"
+                }
+            },
+            "required": ["item"]
+        }
+    }
+
 
     client = genai.Client(api_key=api_key)
 
+    print("Original todos: ", todos)
     # Single interaction containing the whole response
-    # interaction = client.interactions.create(
-    #     model="gemini-3.5-flash",
-    #     input="Explain how AI works in a few words"
-    # )
-    # print(interaction.output_text)
+    interaction = client.interactions.create(
+        model="gemini-3.5-flash",
+        input="Add cycling to my todo list",
+        tools=[add_todo_tool]
+    )
+    for step in interaction.steps:
+        if step.type == "function_call":
+            print("Function call found")
+            if step.name == "add_todo":
+                print("add_todo called args:")
+                argument = step.arguments.get("item")
+                print(argument)
+                add_todo(argument)
+
+    print("output text: ", interaction.output_text)
+    print("updated todos: ", todos)
 
 
     # Stream of responses
-    stream = client.interactions.create(
-        model="gemini-3.5-flash",
-        input="Explain how AI works in a few words",
-        stream=True
-    )
+    # stream = client.interactions.create(
+    #     model="gemini-3.5-flash",
+    #     input="Explain how AI works in a few words",
+    #     stream=True
+    # )
 
-    for event in stream:
-        print("event type: ", type(event))
-        print("event dir: ", dir(event))
+    # for event in stream:
+    #     print("event type: ", type(event))
+    #     print("event dir: ", dir(event))
     # for event in stream:
 
     #     match event.event_type:
