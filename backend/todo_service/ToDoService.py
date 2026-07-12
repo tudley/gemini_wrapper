c75bc71b6bb72b40163b1eb93d90d15a57d4d9ac
Tom Young <youngthomas1997@gmail.com>
2026-07-12 22:17:03 +0100

    method extracting method data into Gemini compatible tool definition complete

--- backend/todo_service/ToDoService.py
+++ backend/todo_service/ToDoService.py
@@ -1,7 +1,8 @@
 from todo_types import TodoItem
-from typing import Literal
+from typing import Literal, get_origin, get_args
 import datetime
 import inspect
+from docstring_parser import parse, Docstring
 
 class ToDoService:
     def __init__(self):
@@ -10,12 +11,14 @@ class ToDoService:
     def add_item_to_list(
         self, 
         title: str,
-        priority: Literal["high", "medium", "low"]
+        priority: Literal["high", "medium", "low"],
+        complete: bool = False
     ):
         """Adds an item to the user's todo list.
 
         Args:
-            item: The todo item to add.
+            title: The title of the item to be added to the todo list
+            priority: The priority of the item being added to the todo list
 
         Returns:
             Whether the operation succeeded.
@@ -52,8 +55,13 @@ class ToDoService:
             self.add_item_to_list(item)
 
     def add_item_to_list_metadata(self):
+
         method = self.add_item_to_list
         signature = inspect.signature(method)
+
+
+        docstring = inspect.getdoc(method)
+        parsed_docstring = parse(docstring)
 
         tool = {
             "name": method.__name__,
@@ -66,20 +74,56 @@ class ToDoService:
             }
         }
 
-        for key, val in signature.parameters.items():
-            print("key: ", key)
-            print("val: ", val)
+        for arg_title, parameter in signature.parameters.items():
+            print("key: ", arg_title)
+            print("val: ", parameter)
+            arg_type = annotation_to_str(parameter.annotation)
+            arg_description = get_arg_description(arg_title, parsed_docstring)
+            required = True if parameter.default is inspect._empty else False
+            if required:
+                tool['parameters']['required'].append(arg_title)
+            
         
             param_obj = {
-                key: { # title
-                "type": val, # dataType
-                "description": val # derived from docstring
+                arg_title: { # title
+                "type": arg_type, # dataType
+                "description": arg_type # derived from docstring
                 }
             }
-            tool['parameters'][key] = param_obj
+            tool['parameters'][arg_title] = param_obj
 
         return tool
 
+def annotation_to_str(annotation):
+    if annotation == str:
+        return "string"
+
+    if annotation == int:
+        return "integer"
+
+    if annotation == bool:
+        return "boolean"
+
+    if get_origin(annotation) is Literal:
+        values = list(get_args(annotation))
+        return {
+            "type": "string",
+            "enum": values
+        }
+
+    return {"type": "object"}
+
+    return "object"
+
+def get_arg_description(
+    arg_title: str, 
+    parsed_docstring: Docstring
+):
+    for param in parsed_docstring.params:
+        if param.arg_name == arg_title:
+            return param.description
+
+# def is_required(parsed_docstring: Docstring)
 
 client = ToDoService()
 # func = client.add_item_to_list
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-# Gemini Wrapper
+Project Vision
+Goal
 
-A utility repository to interfacve with the Gemini AI model though API calls
+Build a reusable AI backend that can communicate with a frontend and safely interact with application state through backend-defined tools.
+
+The long-term target is an AI-powered Strava coach, but the initial implementation uses a simple todo list to validate the architecture.
